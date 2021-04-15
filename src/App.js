d23f0d65a19943820314ad450d0de86d963212f1
Adrian Kokai <adriankokai@gmail.com>
2021-04-15 19:20:55 +0200

    clear default code and create class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import "materialize-css/dist/css/materialize.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <Router>
+                    <Route exact path='/' component={CheckoutPage} />
+                </Router>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
