e64fb59ca84d561474655964d0bd651e0cf6529b
Adrian Kokai <adriankokai@gmail.com>
2021-04-19 12:34:46 +0200

    create Checkout page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {Checkout as CheckoutPage} from './pages/Checkout';
 import "materialize-css/dist/css/materialize.min.css";
 
 export class App extends Component {
