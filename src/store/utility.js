249643aec31b37375a469ee6baab561c09e3f11b
Adrian Kokai <adriankokai@gmail.com>
2021-04-22 16:48:08 +0200

    switch BACKEND_URL

--- src/store/utility.js
+++ src/store/utility.js
@@ -5,5 +5,5 @@ export const updateObject = (state, updatedObject) => {
     })
 }
 
-export const BACKEND_URL = 'http://127.0.0.1:8000/'
-//export const BACKEND_URL = 'https://kokiestore.herokuapp.com/'
+//export const BACKEND_URL = 'http://127.0.0.1:8000/'
+export const BACKEND_URL = 'https://bluemarket.pythonanywhere.com/'
